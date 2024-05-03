49d621df81e959110c222db61dde83feb2022264
nandanugg <nandanugg@gmail.com>
2024-05-03 08:12:14 +0700

    update

--- testCases/manageCat.js
+++ testCases/manageCat.js
@@ -166,7 +166,7 @@ export function TestGetManageCat(config, user, cat, tags = {}) {
 
 
   currentTest = 'search by gender'
-  res = testGet(route, { sex: cat.gender }, headers, tags);
+  res = testGet(route, { sex: cat.sex }, headers, tags);
   assert(res, currentFeature, config, Object.assign(commonChecks(currentTest), {
     'valid payload should have the value equal to gender query param': (r) => isEqual(r, 'data.[].gender', cat.gender),
   }), { sex: cat.gender });
