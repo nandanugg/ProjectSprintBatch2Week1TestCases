bc2adb470bd26a1e35cb022aa9ea15c474a2da86
nandanugg <nandanugg@gmail.com>
2024-05-03 07:25:13 +0700

    fix config

--- config.js
+++ config.js
@@ -1,9 +1,7 @@
 /* eslint-disable no-undef */
 module.exports = {
-    config: {
-        VERBOSE: __ENV.VERBOSE ? true : false,
-        DEBUG_ALL: __ENV.DEBUG_ALL ? true : false,
-        POSITIVE_CASE: __ENV.ONLY_POSITIVE ? true : false,
-        SKIP_PREP_DATA: __ENV.SKIP_PREP_DATA ? true : false,
-    }
+    VERBOSE: __ENV.VERBOSE ? true : false,
+    DEBUG_ALL: __ENV.DEBUG_ALL ? true : false,
+    POSITIVE_CASE: __ENV.ONLY_POSITIVE ? true : false,
+    SKIP_PREP_DATA: __ENV.SKIP_PREP_DATA ? true : false,
 }
