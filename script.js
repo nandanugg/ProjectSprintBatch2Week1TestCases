72e25100f7ba0700364f65a35b5bbcc9a484ed9f
nandanugg <nandanugg@gmail.com>
2024-05-04 13:33:18 +0700

    update

--- script.js
+++ script.js
@@ -345,12 +345,12 @@ export default function () {
         for (let index = 0; index < 3; index++) {
             user = TestRegistration(config);
             user = TestLogin(config, user);
-            console.log("user credentials:", user)
             let cat = TestPostManageCat(config, user);
             TestGetManageCat(config, user, cat);
             TestPutManageCat(config, user);
             TestDeleteManageCat(config, user);
         }
+        console.log("user credentials:", user)
 
         TestPostManageCatMatch(config, user);
         TestGetManageCatMatch(config, user);
